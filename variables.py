3d3e2226923507a39ab03bda31ea7b0ec2f776d5
Fernando <fh1273509@gmail.com>
2023-04-28 23:39:33 -0300

    tipos de variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+nombre = "fernando"
+numero = 20
+numero = 20.5
+encendido = False
+
+print("nombre del alumno: " + nombre)
+
+#ahora tambien podemos especificar el tipo de variable
+edad = int(20)
+print(edad)
+
+caract = str("jose")
+print(caract)
+
+print(type(caract))
