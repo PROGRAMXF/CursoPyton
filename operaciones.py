1dbf172e5a03875a5e9808986a4f49e86ed0a9a2
Fernando <fh1273509@gmail.com>
2023-04-29 22:07:02 -0300

    las cuatro operaciones matematicas

---
+++ operaciones.py
@@ -0,0 +1,11 @@
+n1 = float(input("Ingrese el primer numero: "))
+n2 = float(input("Ingrese el segundo numero: "))
+
+sum = n1 + n2
+rest = n1 - n2
+mult = n1 * n2
+div = n1 / n2
+
+
+print("Los resultados de las cuatro operaciones son: ", sum, rest, mult, div)
+
