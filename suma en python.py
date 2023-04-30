55977797b797dbd922f8ca6e1d2b417868d87a8d
Fernando <fh1273509@gmail.com>
2023-04-29 21:59:52 -0300

    operacion suma y el resto de las operaciones

---
+++ suma en python.py
@@ -0,0 +1,7 @@
+n1 = int(input("Ingrese el primer numero: "))
+n2 = int(input("Ingrese el segundo numero: "))
+
+sum = n1 + n2
+print("La suma de n1 + n2 es: ", sum)
+
+#lo mismo podemos hacer con la resta, la multiplicacion y la division
