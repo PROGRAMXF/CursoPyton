868af35395a92478b8bfa044d8d0920d873b0e9a
Fernando <fh1273509@gmail.com>
2023-04-29 22:21:35 -0300

    area de un circulo

---
+++ areaCirculo.py
@@ -0,0 +1,10 @@
+import math
+
+print("Ingrese el radio del circulo: ")
+
+r = float(input()) #capturamos el radio
+a = math.pi * (r * r)
+pi = math.pi
+
+print("El valor de pi es: ", pi)
+print("El area del circulo es: ", r, "Es: ", round(a, 2) )
