2dbc996bcf61ab9d44f55cd8f62f75163ac1837c
Fernando <fh1273509@gmail.com>
2023-04-30 23:40:26 -0300

    calculamos raiz cuadrada y porcentaje d eun numero

---
+++ calculoPorcentaje.py
@@ -0,0 +1,11 @@
+import math
+
+print("Ingrese el porcentaje que quiere calcular")
+
+porc = float(input())
+print("Ingrese el numero base")
+
+num = float(input())
+result = num * porc / 100
+
+print("El porcentaje es: ", result)
---
+++ raizCuadrada.py
@@ -0,0 +1,16 @@
+import math
+
+"""num = float(input("Ingrese un numero: "))
+
+raiz = math.sqrt(num)
+print(raiz)"""
+
+
+
+num = float(input("Ingrese un numero: "))
+
+raiz = num** 5
+print(raiz)
+
+
+#calculamos la raiz cuadrada
