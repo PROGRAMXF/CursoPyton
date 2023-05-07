a226a596c015a5f2afbba4d374305c6ce8274f32
Fernando <fh1273509@gmail.com>
2023-05-07 19:46:48 -0300

    saber si es par o impar

---
+++ parEimpar.py
@@ -0,0 +1,13 @@
+"""
+detemrinar si un numero es par o impar
+
+"""
+
+print("Ingrese un numero: ")
+num = int(input())
+
+if(num % 2 == 0):
+    print("El numero es par")
+
+else:
+    print("El numero es impar")
