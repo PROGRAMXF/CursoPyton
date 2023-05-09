3fc73130c70d3d484bc840fcdcd3634d47d1fd57
Fernando <fh1273509@gmail.com>
2023-05-09 20:24:28 -0300

    ordenar los numeros

---
+++ ordenarNumeros.py
@@ -0,0 +1,30 @@
+
+print("Ingresar el primer numero: ")
+num1 = int(input())
+
+print("Ingresar el segundo numero: ")
+num2 = int(input())
+
+print("Ingresar el tercer numero: ")
+num3 = int(input())
+
+if(num1 > num2 and num1 > 3):
+    print("", num1, "_", num2, "_", num3)
+
+elif(num2 > num1 and num1 > num3):
+    print("", num2, "_", num1, "_", num3)
+
+elif(num1 > num2 and num2 > num1):
+    print("", num3, "_", num1, "_", num2)
+
+elif(num3 > num2 and num2 > num1):
+    print("", num3, "_", num2, "_", num1)
+
+elif(num1 > num3 and num3 > num2):
+    print("", num1, "_", num3, "_", num2)
+
+elif(num2 > num3 and num3 > num1):
+    print("", num2, "_", num3, "_", num1)
+
+else:
+    print("Se ingresaron numeros iguales!")
