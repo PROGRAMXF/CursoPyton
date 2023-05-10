da4264f38c200d27403b41d7dd7d4df2e04e92b2
Fernando <fh1273509@gmail.com>
2023-05-10 20:12:09 -0300

    promedio de notas

---
+++ promedioNotas.py
@@ -0,0 +1,12 @@
+"""
+escribir un programa para sacar el promedio de tres notas de estudiantes
+
+"""
+
+nota1 = float(input("Ingresar la primer nota: "))
+nota2 = float(input("Ingresar la segunda nota: "))
+nota3 = float(input("Ingresar la tercer nota: "))
+
+prom = (nota1 + nota2 + nota3) / 3
+
+print("El promedio de las tres notas del estudiante es: ", prom)
