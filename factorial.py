ca5edc7a1cf2b17a732996fa87925f1e3d831897
Fernando <fh1273509@gmail.com>
2023-05-11 22:09:51 -0300

    factorial

---
+++ factorial.py
@@ -0,0 +1,12 @@
+"""
+calcular el factorial de un numero
+"""
+
+print("Ingrese un numero: ")
+num = int(input())
+fact = 1
+
+for i in range(1, num+1):
+    fact*= i
+
+print("El factorial de : ", num, "es: ", fact)
