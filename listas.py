f02a2d16d12bef000473e324da9a0a99eb0c8da4
Fernando <fh1273509@gmail.com>
2023-05-14 21:44:48 -0300

    listas

---
+++ listas.py
@@ -0,0 +1,12 @@
+#definimos una lista de paises
+
+paises = ["colombia", "peru", "argentina", "bolivia", "uruguay"]
+#print("Lista de paises: ", paises[0])
+
+i = 0
+
+while i < len(paises):
+    print(paises[i])
+
+    i+= 1
+
