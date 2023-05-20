f356f227dd87249e04c773c1d7b8832bfb640450
Fernando <fh1273509@gmail.com>
2023-05-20 13:50:18 -0300

    clave y contraseña

---
+++ validarUsuarioYContraseña.py
@@ -0,0 +1,14 @@
+user_ = "admin"
+contraseña_ = "123"
+
+print("Bienvenido, ingrese sus datos")
+print("Usuario: ")
+user = input()
+print("Contrseña: ")
+contraseña = input()
+
+if user==user_ and contraseña==contraseña_:
+    print("Bienvenido al programa")
+
+else:
+    print("Ingreso no valido")
