46442c5ff7cd79cde597a4bdce2ab34928038f57
Fernando <fh1273509@gmail.com>
2023-05-20 17:37:48 -0300

    ciclo for

---
+++ cicloFor.py
@@ -0,0 +1,20 @@
+
+n = int(input("Ingrese un numero: "))
+for i in range(n):
+    print("Repeticiones: ", i)
+
+#Con texto:
+
+for i in "Argentina":
+    print(i)
+
+#Ahora recorriendo un array:
+dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+
+for i in dias:
+    print("Los dias de la semana son: ", i)
+
+#Manipulando datos:
+for i in range(0,10,2):
+    print("Repeticiones: ", i)
+
