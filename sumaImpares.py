f0ef12bdbbee3c3a3e89ddcc0ff32461c6e4b1a0
Fernando <fh1273509@gmail.com>
2023-05-28 21:04:21 -0300

    suma de numeros impares

---
+++ sumaImpares.py
@@ -0,0 +1,22 @@
+
+list = []
+num = int(input("Cuantos numeros deseas sumar: "))
+i = 1
+
+
+while i <= num:
+    numero = int(input("Ingrese un numero: "))
+    list.append(numero)
+
+    i+=1
+
+    suma = 0
+    j = 0
+
+    while j < len(list):
+        if list[j] % 2 == 1:
+            suma+=list[j]
+        j+=1
+
+print(f"La suma es:  {suma}")
+      
