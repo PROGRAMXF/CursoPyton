61e2e652cf676ac0a168af965f4027e90ebe9e7c
Fernando <fh1273509@gmail.com>
2023-05-31 21:37:08 -0300

    contar palabra repetida

---
+++ repitePalabra.py
@@ -0,0 +1,8 @@
+lista = ["Colombia", "Argentina", "Peru", "Brasil", "Bolivia", "Peru", "Peru"]
+
+#print(lista.count("Peru"))
+
+#tambien:
+pal = input("Ingrese una palabra: ")
+rep = lista.count(pal)
+print("Encontrada ", rep, "veces.")
